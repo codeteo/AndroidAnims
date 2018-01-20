e995dbe15f9e033a0911abbf8a7e68276f48631f
teo <ramones.teo@gmail.com>
2018-01-20 15:53:02 +0200

    Adds layout for ViewPropertyAnimActivity

--- app/src/main/java/com/anim/androidanimations/ViewPropertyAnimActivity.java
+++ app/src/main/java/com/anim/androidanimations/ViewPropertyAnimActivity.java
@@ -4,14 +4,11 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
-/**
- * Created by css on 1/20/18.
- */
-
 public class ViewPropertyAnimActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_property);
     }
 }
