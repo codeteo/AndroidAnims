0bd6b68318ccedb82606cf5148e140691d730a31
teo <ramones.teo@gmail.com>
2018-01-20 15:43:16 +0200

    Initial commit

---
+++ app/src/main/java/com/anim/androidanimations/ViewPropertyAnimActivity.java
@@ -0,0 +1,13 @@
+package com.anim.androidanimations;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by css on 1/20/18.
+ */
+
+public class ViewPropertyAnimActivity extends AppCompatActivity {
+
+
+
+}
