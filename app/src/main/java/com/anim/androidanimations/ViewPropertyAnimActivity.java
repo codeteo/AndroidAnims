86e0580af8b6b15c19f4e528588ce30f9edd656d
teo <ramones.teo@gmail.com>
2018-01-20 15:50:15 +0200

    Adds ViewPropertyAnimActivity

--- app/src/main/java/com/anim/androidanimations/ViewPropertyAnimActivity.java
+++ app/src/main/java/com/anim/androidanimations/ViewPropertyAnimActivity.java
@@ -1,5 +1,7 @@
 package com.anim.androidanimations;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -8,6 +10,8 @@ import android.support.v7.app.AppCompatActivity;
 
 public class ViewPropertyAnimActivity extends AppCompatActivity {
 
-
-
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
