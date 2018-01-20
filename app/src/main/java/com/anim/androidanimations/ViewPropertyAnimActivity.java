34cc1b44101dbe90c6340cd02f9567544041935b
teo <ramones.teo@gmail.com>
2018-01-20 15:58:11 +0200

    Enables Java 8 language features

--- app/src/main/java/com/anim/androidanimations/ViewPropertyAnimActivity.java
+++ app/src/main/java/com/anim/androidanimations/ViewPropertyAnimActivity.java
@@ -3,12 +3,19 @@ package com.anim.androidanimations;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 public class ViewPropertyAnimActivity extends AppCompatActivity {
+
+    EditText etProfession, etCompany;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_property);
+
+        etProfession = findViewById(R.id.et_profession);
+        etCompany = findViewById(R.id.et_company);
+
     }
 }
