3c48b40a8563ade547b25b65a36ad9697ed5ef05
teo <ramones.teo@gmail.com>
2018-01-31 16:31:20 +0200

    Adds ConstraintAnimActivity

--- app/src/main/java/com/anim/androidanimations/MainActivity.java
+++ app/src/main/java/com/anim/androidanimations/MainActivity.java
@@ -3,25 +3,27 @@ package com.anim.androidanimations;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnViewProperty;
+    private Button btnViewProperty, btnConstraintAnim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnViewProperty = findViewById(R.id.button);
-        btnViewProperty.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ViewPropertyAnimActivity.class);
-                startActivity(intent);
-            }
+        btnViewProperty = findViewById(R.id.btn_property_anim);
+        btnViewProperty.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, ViewPropertyAnimActivity.class);
+            startActivity(intent);
+        });
+
+        btnConstraintAnim = findViewById(R.id.btn_constraint_anim);
+        btnConstraintAnim.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, ConstraintAnimActivity.class);
+            startActivity(intent);
         });
 
     }
