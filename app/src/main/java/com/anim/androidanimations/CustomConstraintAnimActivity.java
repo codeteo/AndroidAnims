115a2f3e2ff9a49130c2d5fa88da6b962703c6ac
teo <ramones.teo@gmail.com>
2018-01-31 18:21:21 +0200

    Adds layout for CustomConstraint Activity

--- app/src/main/java/com/anim/androidanimations/CustomConstraintAnimActivity.java
+++ app/src/main/java/com/anim/androidanimations/CustomConstraintAnimActivity.java
@@ -2,13 +2,19 @@ package com.anim.androidanimations;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 
 public class CustomConstraintAnimActivity extends AppCompatActivity {
+
+    private ConstraintLayout constraintLayout;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_constraint);
+
+        constraintLayout = findViewById(R.id.constraint_custom);
+
     }
 }
