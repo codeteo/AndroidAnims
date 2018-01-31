664fb1034e1a768f6811dd2c19dca4a353429771
teo <ramones.teo@gmail.com>
2018-01-31 18:25:25 +0200

    Updates ids for EditTexts

--- app/src/main/java/com/anim/androidanimations/CustomConstraintAnimActivity.java
+++ app/src/main/java/com/anim/androidanimations/CustomConstraintAnimActivity.java
@@ -4,10 +4,12 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 public class CustomConstraintAnimActivity extends AppCompatActivity {
 
     private ConstraintLayout constraintLayout;
+    private EditText et
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -15,6 +17,8 @@ public class CustomConstraintAnimActivity extends AppCompatActivity {
         setContentView(R.layout.activity_custom_constraint);
 
         constraintLayout = findViewById(R.id.constraint_custom);
+
+
 
     }
 }
